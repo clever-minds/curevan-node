fe62a7ad955131cc004c751c6c730e0f0ef4181b
ujjawal <ujjawal.infonex@gmail.com>
2026-03-04 10:46:32 +0530

    solve cross error

--- src/app.js
+++ src/app.js
@@ -5,9 +5,18 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
+// app.use(cors({
+//   origin: "http://localhost:3000",
+//   credentials: true   // 🔥 REQUIRED
+// }));
+
 app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true   // 🔥 REQUIRED
+  origin: [
+    "https://your-app.vercel.app",
+    "https://13.235.132.236",
+    "http://localhost:3000",
+  ],
+  credentials: true
 }));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
