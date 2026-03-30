03c898f09ccf96a34a89a46abfb51ff195836148
ujjawal <ujjawal.infonex@gmail.com>
2026-03-30 10:43:04 +0530

    google loging call back url changes

--- src/config/passport.js
+++ src/config/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:5000/api/auth/google/callback',
+      callbackURL: 'https://13.234.126.66/api/auth/google/callback',
     },
    async (accessToken, refreshToken, profile, done) => {
         const transaction = await sequelize.transaction();
