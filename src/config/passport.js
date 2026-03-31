8e094691bdc9ed806577a2e96ea63e6bbd6da8d3
ujjawal <ujjawal.infonex@gmail.com>
2026-03-31 17:12:51 +0530

    redirect url problem

--- src/config/passport.js
+++ src/config/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://13.234.126.66/api/auth/google/callback',
+      callbackURL: 'https://api.curevan.com/api/auth/google/callback',
     },
    async (accessToken, refreshToken, profile, done) => {
         const transaction = await sequelize.transaction();
