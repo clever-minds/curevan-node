a574e312e03e30fdc3094d135e1677c56090f6a8
ujjawal <ujjawal.infonex@gmail.com>
2026-06-03 12:25:32 +0530

    fix: Correct controller import path for Linux

--- src/routes/journal_tag.routes.js
+++ src/routes/journal_tag.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Tag = require("../controllers/Tag/JournalTagController");
+const Tag = require("../controllers/category/JournalTagController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const responseHandler = require("../middlewares/responseHandler");
 
